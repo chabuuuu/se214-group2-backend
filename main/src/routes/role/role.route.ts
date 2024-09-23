570c83dacad28334313f5c21d0c974f81ab4f123
chabuuuu <haphuthinh332004@gmail.com>
2024-09-23 19:34:57 +0700

    feature(test): husky

--- main/src/routes/role/role.route.ts
+++ main/src/routes/role/role.route.ts
@@ -11,7 +11,9 @@ roleRouter
   .post('/with-account', classValidate(CreateRoleWithAccountReq), roleController.createWithAccount.bind(roleController))
 
   .post('/', classValidate(CreateRoleReq), roleController.common.create.bind(roleController.common))
+
   .put('/update/:id', classValidate(UpdateRoleReq), roleController.common.update.bind(roleController.common))
+  
   .delete('/delete/:id', roleController.common.delete.bind(roleController.common))
 
   .get('/:id', roleController.common.findOne.bind(roleController.common))
