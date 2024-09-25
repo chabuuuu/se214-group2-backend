78630714bde0f02250dcd2f16beb1550da4c8627
chabuuuu <haphuthinh332004@gmail.com>
2024-09-25 10:31:02 +0700

    feat(init source): add auto generate module tool

--- main/src/routes/index.ts
+++ main/src/routes/index.ts
@@ -1,12 +1,14 @@
 import { ErrorCode } from '@/enums/error-code.enums';
-import accountRouter from '@/routes/account/account.route';
-import courseRouter from '@/routes/course/course.route';
-import courseCategoryRouter from '@/routes/course_category/course_category.route';
-import discountRouter from '@/routes/discount/discount.route';
-import roleRouter from '@/routes/role/role.route';
+import accountRouter from '@/routes/account.route';
+import courseRouter from '@/routes/course.route';
+import courseCategoryRouter from '@/routes/course_category.route';
+import discountRouter from '@/routes/discount.route';
+import lecturerRouter from '@/routes/lecturer.route';
+import roleRouter from '@/routes/role.route';
 import BaseError from '@/utils/error/base.error';
 
 export function route(app: any, root_api: string) {
+  app.use(`${root_api}/lecturer`, lecturerRouter);
   app.use(`${root_api}/course-category`, courseCategoryRouter);
   app.use(`${root_api}/discount`, discountRouter);
   app.use(`${root_api}/course`, courseRouter);
